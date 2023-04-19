79c64542354431742d34a39787e467fc42d624ee
shaurya947 <shaurya.arora1@gmail.com>
2023-04-19 18:23:17 -0400

    Part 2, attempt 1.

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
+
+type problem struct {
+	question, answer string
+}
 
 func main() {
 	filenamePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimitPtr := flag.Int("timeLimit", 30, "allowed time limit (in seconds) for completing the quiz")
 	flag.Parse()
 
 	filePtr, err := os.Open(*filenamePtr)
@@ -24,17 +30,34 @@ func main() {
 		log.Fatal(err)
 	}
 
-	numRightAnswers := 0
-	var response string
+	problems := parseCSVRecords(records)
+
+	fmt.Printf("You will have %d seconds to finish the quiz. Press ENTER to begin.", *timeLimitPtr)
+	fmt.Scanln()
+
+	correct := 0
+	go presentQuiz(problems, &correct)
+
+	time.Sleep(time.Duration(*timeLimitPtr) * time.Second)
+	fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseCSVRecords(records [][]string) []problem {
+	problems := make([]problem, len(records))
 	for index, record := range records {
-		question, answer := record[0], record[1]
-		fmt.Printf("Problem #%d: %s = ", index+1, question)
+		problems[index] = problem{question: record[0], answer: record[1]}
+	}
+	return problems
+}
+
+func presentQuiz(problems []problem, numCorrectPointer *int) {
+	var response string
+	for index, problem := range problems {
+		fmt.Printf("Problem #%d: %s = ", index+1, problem.question)
 		fmt.Scanln(&response)
 
-		if strings.EqualFold(answer, strings.TrimSpace(response)) {
-			numRightAnswers++
+		if strings.EqualFold(problem.answer, strings.TrimSpace(response)) {
+			(*numCorrectPointer)++
 		}
 	}
-
-	fmt.Printf("You scored %d out of %d.\n", numRightAnswers, len(records))
 }
